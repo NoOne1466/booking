3c0911000ccd6f9f8ca7274834acdf7d96ccea37
NoOne1466 <hamzasabermahmoud@gmail.com>
2024-07-08 09:13:39 +0300

    booking a room test 4

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require("dotenv");
 
 dotenv.config({ path: `./config.env` });
 
+console.log(process.env);
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION");
   console.log(err.name, err.message);
