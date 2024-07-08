d5d3c82a5781eeebb9c9190473cd8e47e160553f
NoOne1466 <hamzasabermahmoud@gmail.com>
2024-07-08 08:20:49 +0300

    booking a room test 3

--- src/app/orderedRoom/bookingRoomOrder.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const bookingRoomOrderController = require("./bookingRoomOrder.controller");
-
-const router = express.Router();
-router.post("/", bookingRoomOrderController.bookRoom);
-
-module.exports = router;
